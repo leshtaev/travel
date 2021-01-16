18c365c474fae99585f3860f039bf2ef22620724
lescht <lesht.n@gmail.com>
2021-01-17 01:16:01 +0300

    12 places to build a route

--- js/config.js
+++ js/config.js
@@ -1,9 +1,10 @@
 export const categories = {
+    sightseeings: "Sightseeings",
     museums: "Museums",
-    bars: "Bars",
-    restaurants: "Restaurants",
+    theartes: "Theatres",
     parks: "Parks",
-    theartes: "Theartes",
+    restaurants: "Restaurants",
+    bars: "Bars",
     night_clubs: "Night clubs",
 }
 
@@ -11,10 +12,87 @@ export function getPlaces() {
     const data = [
         {
             name: 'Эрмитаж',
-            image: 'photo.png',
-            categories: ['sightsins', categories.museums],
+            image: 'hermitage.png',
+            categories: [categories.sightseeings, categories.museums],
             coordinates: [50, 60],
-            price: 100
-        }
+            price: 500
+        },
+        {
+            name: 'Русский музей',
+            image: 'rus_museum.png',
+            categories: [categories.sightseeings, categories.museums],
+            coordinates: [50, 60],
+            price: 250
+        },
+        {
+            name: 'Мариинский театр',
+            image: 'mariinka.png',
+            categories: [categories.sightseeings, categories.theartes],
+            coordinates: [50, 60],
+            price: 1000
+        },
+        {
+            name: 'АХЕ',
+            image: 'ahe.png',
+            categories: [categories.theartes],
+            coordinates: [50, 60],
+            price: 400
+        },
+        {
+            name: 'Летний сад',
+            image: 'summertime.png',
+            categories: [categories.sightseeings, categories.parks],
+            coordinates: [50, 60],
+            price: 0
+        },
+        {
+            name: 'Новая Голландия',
+            image: 'new_holland.png',
+            categories: [categories.sightseeings, categories.parks],
+            coordinates: [50, 60],
+            price: 0
+        },
+        {
+            name: 'Столовая №1',
+            image: 'stolovaya_1.png',
+            categories: ['Food', categories.restaurants],
+            coordinates: [50, 60],
+            price: 400
+        },
+        {
+            name: 'Косуля',
+            image: 'cosulya.png',
+            categories: ['Food', categories.restaurants],
+            coordinates: [50, 60],
+            price: 800
+        },
+        {
+            name: 'Цветочки',
+            image: 'tsvetochki.png',
+            categories: ['Drinks', 'Food', categories.bars],
+            coordinates: [50, 60],
+            price: 800
+        },
+        {
+            name: 'Большой',
+            image: 'bolshoy.png',
+            categories: ['Drinks', 'Food', categories.bars],
+            coordinates: [50, 60],
+            price: 1200
+        },
+        {
+            name: 'Central Station',
+            image: 'non_traditional.png',
+            categories: ['Drinks', categories.night_clubs],
+            coordinates: [50, 60],
+            price: 500
+        },
+        {
+            name: 'Stackenschneider',
+            image: 'stackenschneider.png',
+            categories: ['Drinks', categories.night_clubs],
+            coordinates: [50, 60],
+            price: 500
+        },
     ]
 }
