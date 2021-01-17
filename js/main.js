4d002ad10e2d63e62136c4958a3b7b0ddf668c70
lescht <lesht.n@gmail.com>
2021-01-17 19:32:49 +0300

    Selected places shown map

--- js/main.js
+++ js/main.js
@@ -14,22 +14,28 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZGltYWRvYnIiLCJhIjoiY2tqenN3MnF2MGFyejJvbW5sM
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [30, 60],
+    center: [30.315868, 59.939095],
     zoom: 10
 });
-var marker = new mapboxgl.Marker().setLngLat([30, 60]).addTo(map);
+// var marker = new mapboxgl.Marker().setLngLat([30, 60]).addTo(map);
+
+function fillMap(places) {
+    places.forEach((place) => {
+        new mapboxgl.Marker().setLngLat(place["coordinates"].slice().reverse()).addTo(map);
+    })
+}
 
 function openMap() {
     document.getElementById('map').style.visibility = "visible";
     document.querySelector(".close_second").style.visibility = "visible"
 }
 
-// profile data filling
+// profile data filling and map opening
 document.getElementById('action').addEventListener("click", fillProfile)
 document.addEventListener('DOMContentLoaded', () => { 
     document.getElementById('btn').addEventListener('click', (ev) => {
         addUser(ev);
-        selectPlaces(JSON.parse(localStorage.getItem("MyData")));
+        fillMap(selectPlaces(JSON.parse(localStorage.getItem("MyData"))));
         openMap()
     });
 });
