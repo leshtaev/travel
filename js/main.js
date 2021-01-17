0628d8f1866e2b03890a0c4c08015af8ec2365d8
lescht <lesht.n@gmail.com>
2021-01-17 19:00:13 +0300

    Map works

--- js/main.js
+++ js/main.js
@@ -9,28 +9,34 @@ document.querySelector('.close').addEventListener("click", function() {
     document.querySelector(".popup").style.display = "none";
 })
 
-// profile data filling
-document.getElementById('action').addEventListener("click", fillProfile)
-document.addEventListener('DOMContentLoaded', () => { 
-    document.getElementById('btn').addEventListener('click', (ev) => {
-        addUser(ev);
-        selectPlaces(JSON.parse(localStorage.getItem("MyData")))
-    });
-});
-
 // map
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGltYWRvYnIiLCJhIjoiY2tqenN3MnF2MGFyejJvbW5sMWdqaDlzNSJ9.zPzzW8fvyAs_xMkEgWTSfw';
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [59.939095, 30.315868],
+    center: [30, 60],
     zoom: 10
 });
-var marker = new mapboxgl.Marker().setLngLat([12.550343, 55.665957]).addTo(map);
+var marker = new mapboxgl.Marker().setLngLat([30, 60]).addTo(map);
 
-document.getElementById('btn').addEventListener("click", function() {
-    document.querySelector(".close_second").style.visibility = "visible";
-})
+function openMap() {
+    document.getElementById('map').style.visibility = "visible";
+    document.querySelector(".close_second").style.visibility = "visible"
+}
+
+// profile data filling
+document.getElementById('action').addEventListener("click", fillProfile)
+document.addEventListener('DOMContentLoaded', () => { 
+    document.getElementById('btn').addEventListener('click', (ev) => {
+        addUser(ev);
+        selectPlaces(JSON.parse(localStorage.getItem("MyData")));
+        openMap()
+    });
+});
+
+// document.getElementById('btn').addEventListener("click", function() {
+//     document.querySelector(".close_second").style.visibility = "visible";
+// })
 document.querySelector('.close_second').addEventListener("click", function() {
     document.querySelector(".close_second").style.visibility = "hidden";
 })
